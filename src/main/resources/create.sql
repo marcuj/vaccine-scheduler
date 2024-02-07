89d725c6dce0b166e8c6136038b2214e7e2258fd
marcuj <159207730+marcuj@users.noreply.github.com>
2024-02-06 18:13:46 -0800

    Added SQL table design

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -16,3 +16,20 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (
+	Username varchar(255),
+	Salt BINARY(16),
+	Hash BINARY(16),
+	PRIMARY KEY (Username)
+);
+	
+CREATE TABLE Appointments (
+	id_num int IDENTITY(1,1),
+	Time date,
+	User_caregiver varchar(255) REFERENCES Caregivers,
+	User_patient varchar(255) UNIQUE REFERENCES Patients,
+	Vaccine varchar(255) REFERENCES Vaccines,
+	PRIMARY KEY (id_num)
+);
+	
